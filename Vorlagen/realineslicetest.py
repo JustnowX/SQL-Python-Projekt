bbe28a3acd5473d3b98623e9ba64b48a6dc3fa68
kamidor <kamidor@outlook.com>
2020-03-21 13:58:15 +0100

    g

--- Vorlagen/realineslicetest.py
+++ Vorlagen/realineslicetest.py
@@ -3,7 +3,7 @@ config = open("lxc.conf", "r")
 # s = "".join(config.read())
 
 
-x = slice(6, 11)
+x = slice(6, 20)
 
 
 
@@ -20,24 +20,27 @@ line9 = config.readline()
 
 arch = line1[slice(6, 20)]
 
-cores = line2[slice(6, 11)]
-hostname  = line3[slice(6, 11)]
-memory  = line4[slice(6, 11)]
-ostype  = line6[slice(6, 11)]
-vmid = line7[slice(1, 10)]
-swap  = line8[slice(6, 11)]
-unprivileged  = line9[slice(6, 11)]
+cores = line2[slice(7, 20)]
+hostname  = line3[slice(10, 40)]
+memory  = line4[slice(8, 20)]
+net = line5[slice(23, 28)]
+ip = line5[slice(43, 56)]
+ostype  = line6[slice(8, 40)]
+vmid = line7[slice(18, 24)]
+hdd = line7[slice(37, 45)]
+swap  = line8[slice(6, 20)]
+unprivileged  = line9[slice(14, 20)]
 
 print(arch)
 print(cores)
 print(hostname)
 print(memory)
+print(net)
+print(ip)
 print(ostype)
 print(vmid)
+print(hdd)
 print(swap)
 print(unprivileged)
 
-
-
-print(arch)
 config.close()
