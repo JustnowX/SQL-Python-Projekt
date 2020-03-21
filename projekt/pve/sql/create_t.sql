26f3662cd5805fec31f7702f23d81bf03e5fa7ba
JustnowX <60323328+JustnowX@users.noreply.github.com>
2020-03-21 14:43:06 +0100

    update

--- projekt/pve/sql/create_t.sql
+++ projekt/pve/sql/create_t.sql
@@ -1,4 +1,5 @@
-REM @C:\Users\Surface\Python\projekt\pve\sql\create_t.sql
+REM @C:\Users\Surface\Documents\GitHub\SQL-Python-Projekt\projekt\pve\sql\create_t.sql
+C:\Users\Surface\Documents\GitHub\SQL-Python-Projekt\projekt\pve\sql
 
 set NULL 'N/A'
 set linesize 200
@@ -7,21 +8,23 @@ cl scr
 
 conn ora1/ora1
 
+
+
 CREATE TABLE t_host
 (
-  hid     NUMBER(3)     CONSTRAINT conhostid_thost PRIMARY KEY,
+  hid     VARCHAR2(5)     CONSTRAINT conhostid_thost PRIMARY KEY,
   fqdn    VARCHAR2(30)  CONSTRAINT nn_confqdn NOT NULL
                         CONSTRAINT uni_confqdn UNIQUE
 );
 
 CREATE TABLE t_client
 (
-  cid     NUMBER(3)     CONSTRAINT conclientid_tclient PRIMARY KEY,
+  cid     VARCHAR2(5)     CONSTRAINT conclientid_tclient PRIMARY KEY,
   fqdn    VARCHAR2(30)  CONSTRAINT confqdn_nn_tclient NOT NULL
                         CONSTRAINT confqdn_uniq_tclient UNIQUE,
   ip      VARCHAR2(30)  CONSTRAINT conip_tclient UNIQUE,
   sysart  VARCHAR2(30)  CONSTRAINT consysart_nn_tclient NOT NULL,
-  hid     NUMBER(3),
+  hid     VARCHAR2(5),
   CONSTRAINT hostid_thost_tclient FOREIGN KEY (hid) REFERENCES t_host (hid)
 );
 
@@ -33,6 +36,5 @@ INSERT
 INSERT
   INTO t_client (cid, fqdn,            ip,               sysart,  hid)
   VALUES        (101, 'testclient.xxx', '192.168.1.101', 'debian', 111);
-
-  COMMIT;
 */
+  COMMIT;
